7ddeec062fe1c9d4b6ae4268b4172e63fbf2694f
Diana Barsan <barsan@medic.org>
2022-10-18 14:40:27 +0300

    expose 5986 port, update cluster setup script.
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- couchdb/set-up-cluster.sh
+++ couchdb/set-up-cluster.sh
@@ -2,10 +2,11 @@
 set -e
 WAIT_THRESHOLD="${WAIT_THRESHOLD:-20}"
 SLEEP_SECONDS="${SLEEP_SECONDS:-5}"
-NODE_COUNT=3
+CLUSTER_PEERS=(${CLUSTER_PEER_IPS//,/ })
+NODE_COUNT=$((${#CLUSTER_PEERS[@]}+1))
 
 verify_membership(){
-    curl -s http://$COUCHDB_USER:$COUCHDB_PASSWORD@$SVC_NAME:5984/_membership | jq ".all_nodes == .cluster_nodes and (.all_nodes | length) === $NODE_COUNT"
+    curl -s http://$COUCHDB_USER:$COUCHDB_PASSWORD@$SVC_NAME:5984/_membership | jq "(.all_nodes | length) == $NODE_COUNT"
 }
 
 enable_cluster(){
@@ -13,8 +14,6 @@ enable_cluster(){
 
     echo "Cluster already setup">&2
 
-   # elif [ ! -z $( verify_membership | grep cluster_finished ) ]; then
-   # echo "Cluster Setup Already Finished">&2
     else
     curl -X POST -H "Content-Type: application/json" http://$COUCHDB_USER:$COUCHDB_PASSWORD@$SVC_NAME:5984/_cluster_setup \
     -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "'$COUCHDB_USER'", "password":"'$COUCHDB_PASSWORD'", "node_count":"'$NODE_COUNT'"}'
@@ -22,18 +21,18 @@ enable_cluster(){
 }
 
 enable_cluster_on_remote_node(){
+    peer=$1
     curl -X POST -H "Content-Type: application/json" http://$COUCHDB_USER:$COUCHDB_PASSWORD@$SVC_NAME:5984/_cluster_setup \
-    -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "'$COUCHDB_USER'", "password":"'$COUCHDB_PASSWORD'", "port": 5984, "node_count": "'$NODE_COUNT'", "remote_node": "'$REMOTE_COUCHDB_NODE_FQDN'", "remote_current_user": "'$COUCHDB_USER'", "remote_current_password": "'$COUCHDB_PASSWORD'" }'
+    -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "'$COUCHDB_USER'", "password":"'$COUCHDB_PASSWORD'", "port": 5984, "node_count": "'$NODE_COUNT'", "remote_node": "'$peer'", "remote_current_user": "'$COUCHDB_USER'", "remote_current_password": "'$COUCHDB_PASSWORD'" }'
 }
 
 join_node_to_cluster(){
+    peer=$1
     curl -X POST -H "Content-Type: application/json" http://$COUCHDB_USER:$COUCHDB_PASSWORD@$SVC_NAME:5984/_cluster_setup \
-    -d '{"action": "add_node", "host":"'$REMOTE_COUCHDB_NODE_FQDN'", "port":5984, "username": "'$COUCHDB_USER'", "password":"'$COUCHDB_PASSWORD'"}'
-
+    -d '{"action": "add_node", "host":"'$peer'", "port":5984, "username": "'$COUCHDB_USER'", "password":"'$COUCHDB_PASSWORD'"}'
 }
 
-complete_cluster_setup()
-{
+complete_cluster_setup(){
     if $(verify_membership); then
       echo "Cluster Setup Already Finished"
     else
@@ -75,19 +74,17 @@ add_peer_to_cluster(){
     echo "Node $peer is already in the cluster"
     else
       check_if_couchdb_is_ready http://$COUCHDB_USER:$COUCHDB_PASSWORD@$peer:5984
-      enable_cluster_on_remote_node
-      join_node_to_cluster
+      enable_cluster_on_remote_node $peer
+      join_node_to_cluster $peer
     fi
 }
 
 add_peers_to_cluster() {
     for PEER in ${CLUSTER_PEER_IPS//,/ }
     do
-    export REMOTE_COUCHDB_NODE_FQDN=$PEER
     add_peer_to_cluster $PEER
     done
     complete_cluster_setup
-    #verify_cluster_setup
     check_cluster_membership
 }
 
