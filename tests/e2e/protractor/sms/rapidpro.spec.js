a27ee47c12c921cdb5564251ab79a489779581c7
Diana Barsan <barsan@medic.org>
2023-02-24 17:39:33 +0200

    I pay for trying distributed attention

--- tests/e2e/protractor/sms/rapidpro.spec.js
+++ tests/e2e/protractor/sms/rapidpro.spec.js
@@ -626,7 +626,7 @@ describe('RapidPro SMS Gateway', () => {
         // console.log(JSON.stringify(messagesEndpointRequests, null, 2));
         const queriedGatewayRefs = messagesEndpointRequests.map(ref => ref[0].broadcast);
         const missingRefs = docs.filter(doc => !queriedGatewayRefs.includes(doc.tasks[0].gateway_ref));
-        const serverDocs = await utils.getDoc(missingRefs.map(doc => doc._id));
+        const serverDocs = await utils.getDocs(missingRefs.map(doc => doc._id));
         console.log(JSON.stringify(serverDocs, null, 2));
         throw err;
       }
